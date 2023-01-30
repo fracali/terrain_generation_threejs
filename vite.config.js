850335775bf0b681a538e68e0bb4691693d0d3c3
Francesco Califano <fra.cali02@gmail.com>
2023-01-30 11:22:39 +0100

    Dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import glsl from "vite-plugin-glsl";
 export default {
   root: "src",
   build: {
-    outDir: "../public",
+    outDir: "../dist",
     emptyOutDir: true,
   },
   plugins: [glsl()],
