78100b39037b1528bf9f46be32b62c7eb07d2e46
Francesco Califano <fra.cali02@gmail.com>
2023-02-07 12:25:53 +0100

    Terrain color

--- src/js/materials/terrain_mat.ts
+++ src/js/materials/terrain_mat.ts
@@ -2,7 +2,7 @@ import { MeshStandardMaterial } from "three";
 
 export default function (): MeshStandardMaterial {
   const material = new MeshStandardMaterial({
-    color: 0xff8dcb72,
+    color: 0xff6a7638,
     roughness: 0.8,
     metalness: 0.01,
   });
